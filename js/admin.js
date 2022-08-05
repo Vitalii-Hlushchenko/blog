2f2f79caa4b0688506cc1ab7c1d8905538dc8536
Vitalii-Hlushchenko <vitalikglush@gmail.com>
2022-08-05 15:47:14 +0300

    final commit

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,37 @@
+
+// GET DATA// 
+const articleContainer = document.querySelector('#articles-container');
+
+getProducts();
+
+async function getProducts() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+    const productsArray = await response.json();
+	renderProducts(productsArray);
+
+    
+}
+/**RENDER ARTICLES**/ 
+function renderProducts(productsArray) {
+  productsArray.forEach(function (item) {
+      const productHTML = `<div class="article" id="article${item.id}">
+      <p>${item.title}</p>
+      <div class="article-admin-item">
+        <p class="article-text">${item.body}</p>
+        <button class="edit-btn" onclick="location.href='edit article.html'">Edit</button>
+
+        <button onclick="deleteArticle(${item.id})" class="delete-btn">Delete</button>
+      </div>
+    </div>`;
+      articleContainer.insertAdjacentHTML('beforeend', productHTML);
+  });
+}
+
+
+
+
+/*CREATE ARTICLE*/ 
+
 const form = document.querySelector('#create-form');
 
 
@@ -6,11 +40,49 @@ const form = document.querySelector('#create-form');
     e.preventDefault();
     const formData = new FormData(form);
     const payload = new URLSearchParams(formData);
-    fetch('https://jsonplaceholder.typicode.com/users', {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: payload,
     })
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => console.log(data));
     })
+    })();
+
+
+
+/*DELETE ARTICLES*/ 
+
+async function deleteArticle(id){
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/${id}', {
+    method: 'DELETE',
+    headers:{
+      'Content-Type':'application/json',
+    }
+  });
+  const data = await res.json();
+  console.log(data);
+
+  if (data) {
+    document.getElementById(`article${id}`).remove();
+  }
+}
+
+/*EDIT ARTICLE*/ 
+
+
+
+(function editArticle(id) {
+    form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const payload = new URLSearchParams(formData);
+    fetch('https://jsonplaceholder.typicode.com/posts/${id}', {
+    method: 'PATCH',
+    body: payload,
+    
+    })
+    .then(res => res.json())
+    .then(data => console.log(data));
+    });
     })();
--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 
-const articleContainer = document.querySelector('#article-container');
+const articleContainer1 = document.querySelector('#article-container');
 
-getProducts();
+getArticles1();
 
-async function getProducts() {
-    const response = await fetch('https://mockend.com/org/repo/products');
-    const productsArray = await response.json();
-	renderProducts(productsArray);
+async function getArticles1() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=2');
+    const articlesArray = await response.json();
+	renderArticles1(articlesArray);
 
     
 }
 
 
 
-function renderProducts(productsArray) {
-    productsArray.forEach(function (item) {
+function renderArticles1(articlesArray) {
+    articlesArray.forEach(function (item) {
         const productHTML = `<div class="article">
         <div class="article-text">
             <h2  class="article-title" >${item.title}</h2>
-            <p  class="article-description" >${item.desc} </p>
+            <p  class="article-description" >${item.body} </p>
             <div class="article-item">
-                <p  class="publication-date" >${item.datePub}</p>
+                <p  class="publication-date" >May 20th 2020</p>
                 <a href="#">Read more</a>
             </div>
         </div>
@@ -28,8 +28,85 @@ function renderProducts(productsArray) {
             <img src="img/image 1.jpg">
         </div>
     </div>`;
-        articleContainer.insertAdjacentHTML('beforeend', productHTML);
+        articleContainer1.insertAdjacentHTML('beforeend', productHTML);
     });
     
 }
 
+
+const articleContainer2 = document.querySelector('#second-article-container');
+
+getArticles2();
+
+async function getArticles2() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3');
+    const articlesArray = await response.json();
+	renderArticles2(articlesArray);
+
+    
+}
+
+
+
+function renderArticles2(articlesArray) {
+    articlesArray.forEach(function (item) {
+        const productHTML = `<div class="article article--second">
+        <div class="article-img">
+          <img src="img/image 2.jpg" />
+        </div>
+        <div class="article-text second--article-text">
+          <h2 id="title" class="article-title second--article-title">
+          ${item.title}
+          </h2>
+          <p id="desc" class="article-description">
+            ${item.body}
+          </p>
+          <div class="article-item">
+            <p id="datePub" class="publication-date">May 20th 2020</p>
+            <a href="#">Read more</a>
+          </div>
+        </div>
+      </div>`;
+        articleContainer2.insertAdjacentHTML('beforeend', productHTML);
+    });
+    
+}
+
+
+
+const articleContainer3 = document.querySelector('#section-third-article');
+
+getArticles3();
+
+async function getArticles3() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=1');
+    const articlesArray = await response.json();
+	renderArticles3(articlesArray);
+
+    
+}
+
+
+
+function renderArticles3(articlesArray) {
+    articlesArray.forEach(function (item) {
+        const productHTML = `<div class="article article--third">
+        
+        <div class="article-text third--article-text">
+          <h2 id="title" class="article-title third--article-title">${item.title}</h2>
+          <p id="desc" class="article-description">
+            ${item.body}
+          </p>
+          <div class="article-item">
+            <p id="datePub" class="publication-date">May 20th 2020</p>
+            <a href="#">Read more</a>
+          </div>
+        </div>
+        <div class="article-img">
+          <img src="img/image 7.png" />
+        </div>
+      </div>`;
+        articleContainer3.insertAdjacentHTML('beforeend', productHTML);
+    });
+    
+}
